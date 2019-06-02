660a085267e70997f179aed7bf42e181acaea383
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-06-02 22:15:27 +0200

    sherlockValidString not passing one test.

--- src/main/java/stringManipulation/sherlockValidString/Solution.java
+++ src/main/java/stringManipulation/sherlockValidString/Solution.java
@@ -1,12 +1,16 @@
 package stringManipulation.sherlockValidString;
 
 import java.util.HashMap;
+import java.util.IntSummaryStatistics;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 public class Solution {
     static String isValid(String s) {
+
+        if (s.length() < 2) {
+            return "YES";
+        }
+
         Map<Character, Integer> charactersCount = new HashMap<>();
 
         int minCount = 0;
@@ -19,19 +23,29 @@ public class Solution {
             int charCount = charactersCount.merge(c, 1, (V1, V2) -> (++V1));
         }
 
-        int lettersToBeRemoved = 0;
-        int lastLetterCount = 0;
-        int i = 0;
+        IntSummaryStatistics charCountStats = charactersCount.entrySet().stream().mapToInt(Map.Entry::getValue).summaryStatistics();
+        maxValue = charCountStats.getMax();
+        minValue = charCountStats.getMin();
+
         for (Map.Entry<Character, Integer> charCount : charactersCount.entrySet()) {
-            if (i == 0) {
-                lastLetterCount = charCount.getValue();
-                i++;
-            } else {
-                lettersToBeRemoved += Math.abs(lastLetterCount - charCount.getValue());
-                if (lettersToBeRemoved > 1)
-                    return "NO";
+            int count = charCount.getValue();
+
+            if (count == maxValue) {
+                maxCount++;
+            }
+            if (count == minValue) {
+                minCount++;
             }
         }
-        return "YES";
+
+        if (minValue == maxValue)
+            return "YES";
+        if((minCount*minValue + maxCount*maxValue) != s.length())
+            return "NO";
+        if(minValue==1 && minCount==1)
+            return "YES";
+        if(maxCount==1 && maxValue==minValue+1)
+            return "YES";
+        return "NO";
     }
 }
